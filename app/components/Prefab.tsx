7524b37a97d70aa344eb180c8af14739219c670b
TyCaugher <tak4506@gmail.com>
2023-10-07 17:22:37 -0500

    Boilerplate results styling

--- app/components/Prefab.tsx
+++ app/components/Prefab.tsx
@@ -1,10 +1,18 @@
 import { Link } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/node";
+import stylesheet from "../styles/prefablist.css"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
 
 export default function Prefab (props: any) {
     return (
-        <div className="assetResult" key={props._id}>
+        <div className="prefab-list-item" key={props._id}>
             <Link to={`/prefabs/${props._id}`}>
-                {props.title} ({props.artist})
+                <p className="prefab-list-item-title">{props.title} (Artist)</p>
+                <p className="prefab-list-item-price">$0.00</p>
             </Link>
         </div>
     )
--- app/routes/prefabs._index.tsx
+++ app/routes/prefabs._index.tsx
@@ -1,9 +1,14 @@
 import { useLoaderData, Form } from "@remix-run/react";
 import { json } from "@remix-run/node";
-import { LoaderFunctionArgs } from "@remix-run/node";
+import { LoaderFunctionArgs, LinksFunction } from "@remix-run/node";
 import { mongodb } from "~/utils/db.server";
 import type { Prefab } from "../utils/types.server"
 import PrefabComponent from "../components/Prefab"
+import stylesheet from "../styles/prefablist.css"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
     const url = new URL(request.url)
@@ -20,14 +25,31 @@ export default function Prefabs() {
     const prefabs = useLoaderData();
 
     return (
-        <div>
-            <h1 className="text-xl">Prefabs</h1>
+        <div className="prefab-list-screen">
+            <div className="prefab-list">
             {prefabs.map((prefab) => {
                 return (
+                    // Look into passing CSS styles later
                     <PrefabComponent key={prefab._id} {...prefab} />
                 )
             })}
+            </div>
         </div>
     )
 }
-
+/*
+<div class="search-results-screen">
+  <ul class="search-results">
+    <li class="search-result-item">
+      <a class="search-result-item-link" href="#">This is the first search result.</a>
+      <h3 class="search-result-item-title">This is the title of the first search result.</h3>
+      <p class="search-result-item-description">This is a brief description of the first search result.</p>
+    </li>
+    <li class="search-result-item">
+      <a class="search-result-item-link" href="#">This is the second search result.</a>
+      <h3 class="search-result-item-title">This is the title of the second search result.</h3>
+      <p class="search-result-item-description">This is a brief description of the second search result.</p>
+    </li>
+  </ul>
+</div>
+*/
